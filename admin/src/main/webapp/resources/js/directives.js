ebb63e232e6ae058cdb17c1b52bfd807eef5f73f
Fernando Garcia <fernando.garcia@rivetlogic.com>
2013-10-25 14:26:24 -0600

    CRAFTERCMS-346 - Display details of UGC

--- admin/src/main/webapp/resources/js/directives.js
+++ admin/src/main/webapp/resources/js/directives.js
@@ -33,75 +33,48 @@ angular.module('moderationDashboard.directives', []).
                     // updating moderation value
                     var element = elm;
 
-                    elm.find('.entries-list-btn').bind('click', { options: elm.find('.btn-group') }, function (ev) {
-                        var moderationSelected = ev.data.options.find('.active').text(),
-                            queryParams = {
-                                moderationid: element.attr('ugcid'),
-                                moderationstatus : moderationSelected.toUpperCase(),
-                                tenant: scope.$parent.confObj.tenant
-                            };
+                    // handler when a options is selected, changes the status
+                    timeout(function () {
+                        elm.find('.btn-group').delegate('.btn', 'click', function (ev) {
+                            var action = $(ev.currentTarget).val().toUpperCase(),
+                                queryParams = {
+                                    moderationid: element.attr('ugcid'),
+                                    moderationstatus : action,
+                                    tenant: scope.$parent.confObj.tenant
+                                };
 
-                        // validate if any option was selected
-                        if (moderationSelected !== ""){
-                            //TODO change $resource for restangular
                             http({
                                 method: 'POST',
                                 url: "/crafter-social/api/2/ugc/moderation/" + queryParams.moderationid + "/status.json?moderationStatus=" + queryParams.moderationstatus + "&tenant=" + queryParams.tenant,
                                 data: queryParams,
                                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
                             }).success(function (data) {
-                                    var key;
-                                    angular.forEach(scope.$parent.$parent.ugcList, function (ugc, index) {
-                                        if (ugc.id === data.id) {
-                                            key = index;
-                                        }
-                                    });
-                                    if (key !== undefined) {
-                                        scope.$parent.ugcList.splice(key, 1);
-
-                                        //TODO efect for removed item
-                                        scope.$parent.setAlert(
-                                            "UGC updated correctly",
-                                            "alert-success"
-                                        );
+                                var key;
+                                angular.forEach(scope.$parent.$parent.ugcList, function (ugc, index) {
+                                    if (ugc.id === data.id) {
+                                        key = index;
                                     }
-                                }).error(function (data) {
-                                    scope.$parent.setAlert("Error trying to save UGC", "alert-error");
                                 });
-                        }else{
-                            // error message when an action was not selected
-                            var displayError = $(this).popover({
-                                animation: true,
-                                placement: 'right',
-                                trigger: 'manual',
-                                title: 'Error Title',
-                                content: 'Please select an option'
+                                if (key !== undefined) {
+                                    scope.$parent.ugcList.splice(key, 1);
+
+
+
+                                    //TODO efect for removed item
+                                    scope.$parent.setAlert(
+                                        "UGC updated correctly - " + action,
+                                        "alert-success"
+                                    );
+                                }
+                            }).error(function (data) {
+                                scope.$parent.setAlert("Error trying to save UGC", "alert-error");
                             });
 
-                            displayError.popover('show');
-                        }
-                    });
-
-                    // handler when a options is selected and the error message is displayed
-                    timeout(function () {
-                        elm.find('.btn-group').delegate('.btn', 'click', {saveBtn: elm.find('.entries-list-btn')}, function (ev) {
-                            if ($(ev.data.saveBtn).next('div.popover:visible').length){
-                                $(ev.data.saveBtn).popover('hide');
-                            }
                         });
                     });
                 }
             }
         };
-    }]).
-    directive('userProfile', ['Api', function (api) {
-        return {
-            restrict: "E",
-            templateUrl: "/crafter-social-admin/resources/templates/user_profile.html",
-            link: function (scope, elm, attrs) {
-
-            }
-        }
     }]).
     directive('selectallugcs', [function () {
         return function (scope, elm) {
@@ -120,6 +93,37 @@ angular.module('moderationDashboard.directives', []).
             templateUrl: "/crafter-social-admin/resources/templates/error.html",
             link: function (scope, elm, attrs) {
 
+            }
+        }
+    }]).
+    directive('detailInformation', ['$sanitize', function (sanitize) {
+        return {
+            restrict: "A",
+            link: function (scope, elm, attrs) {
+                attrs.$observe('detailInformation', function (val) {
+                    elm.bind('click', function (ev) {
+                        if (elm.hasClass('collapsed')){
+                            angular.forEach(scope.$parent.ugcList, function (ugc) {
+                                if (ugc.id === val) {
+                                    var wholeContent = sanitize(ugc.completeContent);
+
+                                    elm.find(".ugc-content").html(wholeContent);
+                                }
+                            });
+                            elm.toggleClass("collapsed");
+                            elm.toggleClass("expanded");
+                        }else if (elm.hasClass('expanded')) {
+                            angular.forEach(scope.$parent.ugcList, function (ugc) {
+                                if (ugc.id === val) {
+                                    var partialContent = ugc.textContent;
+                                    elm.find(".ugc-content").html(partialContent);
+                                }
+                            });
+                            elm.toggleClass("collapsed");
+                            elm.toggleClass("expanded");
+                        }
+                    });
+                });
             }
         }
     }]);
