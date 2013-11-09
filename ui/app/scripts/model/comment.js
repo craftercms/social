df30d4544c315f13f9d73d1ef818f5e920e68de7
Roy Art <royart@me.com>
2013-11-09 02:34:41 +0100

    * Removing unused defaults * Modifying defaults * textContent now is plain text

--- ui/app/scripts/model/comment.js
+++ ui/app/scripts/model/comment.js
@@ -1,7 +1,7 @@
 (function (S) {
     'use strict';
 
-    var U = S.util,
+    var $ = S.$,
         SERVICE = S.Cfg('url.service');
 
     var Comment = S.Backbone.Model.extend({
@@ -10,34 +10,34 @@
             if (this.isNew()) {
                 return '%@ugc/create.json'.fmt(SERVICE);
             } else {
-                return '%@ugc/like/%@.json'.fmt(SERVICE, this.id);
+                return '%@ugc/like/%@.json'.fmt(SERVICE, this.get('id'));
             }
         },
         defaults: {
             'actions' : [
                 {
                     'name' : 'read',
-                    'roles' : ['SOCIAL_AUTHOR']
+                    'roles' : ['SOCIAL_AUTHOR', 'SOCIAL_ADMIN']
                 },
                 {
                     'name' : 'update',
-                    'roles' : ['SOCIAL_AUTHOR']
+                    'roles' : ['SOCIAL_AUTHOR', 'SOCIAL_ADMIN']
                 },
                 {
                     'name' : 'create',
-                    'roles' : ['SOCIAL_AUTHOR']
+                    'roles' : ['SOCIAL_AUTHOR', 'SOCIAL_ADMIN']
                 },
                 {
                     'name' : 'delete',
-                    'roles' : ['SOCIAL_AUTHOR']
+                    'roles' : ['SOCIAL_AUTHOR', 'SOCIAL_ADMIN']
                 },
                 {
                     'name' : 'act_on',
-                    'roles' : ['SOCIAL_AUTHOR']
+                    'roles' : ['SOCIAL_AUTHOR', 'SOCIAL_ADMIN']
                 },
                 {
                     'name' : 'moderate',
-                    'roles' : ['SOCIAL_AUTHOR']
+                    'roles' : ['SOCIAL_AUTHOR', 'SOCIAL_ADMIN']
                 }
             ],
             'anonymousFlag' : false,
@@ -58,35 +58,31 @@
             'targetId' : '',
             'targetUrl' : '',
             'tenant' : '',
-            'textContent' : '{ "content": "" }',
+            'textContent' : '', // '{ "content": "" }',
             'timesModerated' : 0
         },
-        parse: function (data) {
-            data.content = U.fromJSON(data.textContent).content;
-            return data;
-        },
+//        parse: function (data) {
+//            data.content = U.fromJSON(data.textContent).content;
+//            return data;
+//        },
         like: function () {
             this.save({ likeCount: this.get('likeCount') + 1 }, {
-                url: S.component.Director.actionURL('ugc.like', { id: this.id }),
+                url: S.component.Director.actionURL('ugc.like', { id: this.get('id') }),
                 type: 'POST', // TODO REMOVE OR ADJUST
-                //data: JSON.stringify({ tenant: this.get('tenant'), reason: 'hello' })
+                data: $.param({ tenant: this.get('tenant') })
             });
         },
-        flag: function () {
+        flag: function ( reason ) {
             this.save({ flagCount: this.get('flagCount') + 1 }, {
-                url: S.component.Director.actionURL('ugc.flag', { id: this.id }),
-                type: 'POST' // TODO REMOVE OR ADJUST
+                url: S.component.Director.actionURL('ugc.flag', { id: this.get('id') }),
+                type: 'POST', // TODO REMOVE OR ADJUST
+                data: $.param({ tenant: this.get('tenant'), reason: reason })
             });
         },
         reply: function () {
 
         }
     });
-
-    Comment.DEFAULTS = {
-        tenant: null,
-        target: null
-    };
 
     S.define('model.Comment', Comment);
 
