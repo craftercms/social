b92d5bd0d4bf0b3cdef25a1c6e7cb52066b04e7b
Fernando Garcia <fernando.garcia@rivetlogic.com>
2013-11-18 00:12:27 -0600

    reafactoring popover-message directive

--- admin/src/main/webapp/static-assets/csadmin/js/directives.js
+++ admin/src/main/webapp/static-assets/csadmin/js/directives.js
@@ -2,90 +2,36 @@
 
 /* Directives */
 angular.module('moderationDashboard.directives', []).
-    directive('moderationAction', ['$timeout', 'UgcApi', 'PERMANENTLY_DELETE', 'CONFIG', function (timeout, UgcApi, PD, CONFIG) {
+    directive('moderationAction', ['PERMANENTLY_DELETE', 'CONFIG', 'UgcApi', function (PD, CONFIG, UgcApi) {
         return {
             restrict: "E",
             templateUrl: CONFIG.TEMPLATES_PATH +  "moderation_actions.html",
 
-            scope: {
-                modstatus: '@',
-                ugcid: '@'
-            },
-            link: {
-                pre: function (scope, elm, attrs) {
-                    scope.moderationActions = scope.$parent.moderationActions;
-                },
-                post: function (scope, elm) {
-                    // updating moderation value
-                    var element = elm,
-                        showPopover = function (element) {
-                            if (! $(element).next('div.popover:visible').length) {
-                                $(element).popover({
-                                    animation: true,
-                                    placement: 'right',
-                                    trigger: 'manual',
-                                    'html': true,
-                                    title: 'Are you sure ?',
-                                    content: function (){
-                                        var popover = $(element).next('.popover');
-                                        popover.removeClass('hidden');
-                                        return popover.html();
-                                    }
-                                }).popover('show');
-                            }
+            link: function (scope, elm, attrs) {
+                scope.ugcid = attrs.ugcid;
+
+                var actions = scope.moderationActions;
+                angular.forEach(actions, function (action, i) {
+                    if (action.status.toUpperCase() === PD.ACTION){
+                        action.isDelete = true;
+                    }else{
+                        action.isDelete = false;
+                    }
+                });
+
+                elm.find('.mod-actions-btn').delegate('.btn-mod-action', 'click', function (ev) {
+                    var currentEl = $(ev.currentTarget),
+                        action = currentEl.val().toUpperCase(),
+                        queryParams = {
+                            moderationid: scope.ugcid,
+                            moderationstatus : action,
+                            tenant: scope.confObj.tenant
                         };
 
-                    // handler when a options is selected, changes the status
-                    timeout(function () {
-                        elm.find('.mod-actions-btn').delegate('.btn-mod-action', 'click', function (ev) {
-                            var currentEl = $(ev.currentTarget),
-                                action = currentEl.val().toUpperCase(),
-                                queryParams = {
-                                    moderationid: element.attr('ugcid'),
-                                    moderationstatus : action,
-                                    tenant: scope.$parent.confObj.tenant
-                                };
-
-                            // if action selected is delete / other
-                            if (currentEl.val().toUpperCase() === PD.ACTION) {
-                                showPopover(this);
-                            }else{
-                                UgcApi.updateUgc(queryParams).then(function (data) {
-                                    scope.$parent.displayResults(data, currentEl, { undo: true, message: "" });
-                                });
-                            }
-
-                        });
-
-                        // handler when an ugc is going to be deleted
-                        elm.find('.mod-actions-btn').delegate('.delete-btn', 'click', function (ev) {
-                            var currentEl = $(ev.currentTarget);
-
-                            // if user clicked yes / no
-                            if (currentEl.val().toUpperCase() === PD.CONFIRM) {
-                                var conf = {
-                                    tenant: scope.$parent.confObj.tenant,
-                                    ugcids: [element.attr('ugcid')]
-                                };
-
-                                UgcApi.permanentlyDelete(conf).then(function (data) {
-                                    if (data){
-                                        $(this).popover('destroy');
-                                        scope.$parent.displayResults(data, currentEl, { undo: false, message: "Comment successfully deleted" });
-                                    }else {
-                                        console.log("error trying to delete comment");
-                                        //TODO display message explaining the reason
-                                    }
-                                });
-                            }else{
-                                var popover = $(this).parents('.popover').prev('.active');
-                                popover.removeClass('active');
-                                popover.popover('hide');
-                            }
-                        });
-
+                    UgcApi.updateUgc(queryParams).then(function (data) {
+                        scope.displayResults(data, currentEl, { undo: true, message: "" });
                     });
-                }
+                });
             }
         };
     }]).
@@ -146,4 +92,60 @@ angular.module('moderationDashboard.directives', []).
                 });
             }
         }
+    }]).
+
+    directive('popoverMessage', ['CONFIG', '$timeout', 'PERMANENTLY_DELETE', 'UgcApi', function (CONFIG, timeout, PD, UgcApi) {
+        return {
+            restrict: 'E',
+            templateUrl: CONFIG.TEMPLATES_PATH +  "popover_message.html",
+            link: function (scope, elm, attrs) {
+                scope.ugcid = attrs.ugcid;
+                scope.ugclabel = attrs.ugclabel;
+
+                scope.showPopover = function (event) {
+                    if (! $(event.currentTarget).next('div.popover:visible').length) {
+                        $(event.currentTarget).popover({
+                            animation: true,
+                            placement: 'right',
+                            trigger: 'manual',
+                            'html': true,
+                            title: 'Are you sure ?',
+                            content: function (){
+                                var popover = $(event.currentTarget).next('.popover');
+                                popover.removeClass('hidden');
+                                return popover.html();
+                            }
+                        }).popover('show');
+                    }
+                };
+
+                timeout(function () {
+                    elm.delegate('.delete-btn', 'click', function (ev) {
+                        var target = $(ev.currentTarget);
+                        var popover = $(ev.currentTarget).parents('.popover').prev('.active');
+
+                        if (target.val().toUpperCase() === PD.CONFIRM) {
+                            var conf = {
+                                tenant: scope.confObj.tenant,
+                                ugcids: [scope.ugcid]
+                            };
+
+                            UgcApi.permanentlyDelete(conf).then(function (data) {
+                                if (data){
+
+                                    popover.popover('destroy');
+                                    scope.displayResults(data, currentEl, { undo: false, message: "Comment successfully deleted" });
+                                }else {
+                                    console.log("error trying to delete comment");
+                                    //TODO display message explaining the reason
+                                }
+                            });
+                        }else{
+                            popover.removeClass('active');
+                            popover.popover('hide');
+                        }
+                    });
+                });
+            }
+        };
     }]);
