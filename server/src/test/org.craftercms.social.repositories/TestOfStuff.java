13a18a3b8f3a6783524d85ab31419a5a0a985893
Carlos Ortiz <carlos.ortiz@craftercms.org>
2014-11-19 14:43:12 -0600

    Fixes CRAFTERCMS-915 This changes the way Profile connects to Mongo now uses a property crafter.social.mongodb.connection.connectionStr the format of the String is host:[port],host:[port] if port is not set assumes default (27017)

--- server/src/test/org.craftercms.social.repositories/TestOfStuff.java
+++ server/src/test/org.craftercms.social.repositories/TestOfStuff.java
@@ -1,5 +1,23 @@
+/*
+ * Copyright (C) 2007-2014 Crafter Software Corporation.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.craftercms.social.repositories.ugc.UGCRepository;
 import org.craftercms.social.services.ugc.UGCService;
 import org.junit.Test;
@@ -10,27 +28,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
  *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:/spring/root-context.xml")
+
 public class TestOfStuff {
-    private Logger log = LoggerFactory.getLogger(TestOfStuff.class);
-    @Autowired
-    private UGCService ugcService;
-    @Autowired
-    private UGCRepository repo;
 
     @Test
     public void Test() throws Exception {
-        log.debug("CRAP");
-       assertTrue(Pattern.compile("tenantId:|$where:",Pattern.CASE_INSENSITIVE|Pattern.UNICODE_CASE|Pattern.DOTALL).matcher
-           ("{tenantId:\"Crap\",$where:{}," +
-           "attributes.name:\"\"," +
-           "" + "attributes.tenantId:\"MoreCrap\"}").find());
+        assertEquals(NumberUtils.toInt("25"),25);
 
 
     }
